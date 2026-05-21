c3c43bd3a80a0de5b9551b0e368bf9e57e406936
Leopold Thomasset <leopold@thomasset.fr>
2026-05-21 16:38:45 +0200

    Initial project setup

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server listening on http://localhost:3000");
+});
